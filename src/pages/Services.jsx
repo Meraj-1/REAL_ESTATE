469a16c4f2b59abf8cb5e963cfc7f9fa0284f157
Meraj-1 <belalraza158@gmail.com>
2026-01-03 23:59:49 +0530

    add property details page

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,18 +1,39 @@
-import React from 'react'
-import ServiceExper from '../components/serviceExper'
-import UnlockProp from '../components/UnlockProp'
-import SmartInv from '../components/SmartInv'
+import React from "react";
+import ServiceExper from "../components/serviceExper";
+import UnlockProp from "../components/UnlockProp";
+import SmartInv from "../components/SmartInv";
+
 const Services = () => {
   return (
-    <div>
-      <ServiceExper/>
-    <div className='bg-black max-w-[1790px mx-auto px-4'>
-      <UnlockProp/>
-      <SmartInv/>
-    </div>
+    <main className="bg-black text-white">
 
-    </div>
-  )
-}
+      {/* Hero / Experience Section */}
+      <section className="relative">
+        <ServiceExper />
+      </section>
 
-export default Services
+      {/* Divider Glow */}
+      <div className="h-px w-full bg-gradient-to-r from-transparent via-[#C778DD]/40 to-transparent my-20" />
+
+      {/* Content Wrapper */}
+      <section className="max-w-[1790px] mx-auto px-4 sm:px-8 lg:px-16 space-y-28 pb-32">
+
+        {/* Unlock Properties */}
+        <div className="relative">
+          <UnlockProp />
+        </div>
+
+        {/* Divider */}
+        <div className="h-px w-full bg-gradient-to-r from-transparent via-white/10 to-transparent" />
+
+        {/* Smart Investment */}
+        <div className="relative">
+          <SmartInv />
+        </div>
+
+      </section>
+    </main>
+  );
+};
+
+export default Services;
